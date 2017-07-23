edbd732fd3f6c3bfc8b952f74a7b5509a85e3495
user <vv5126@163.com>
2017-07-23 21:37:22 +0800

    good

---
+++ src/main/java/com/cl/android/music/swipemenulistview/SwipeMenuCreator.java
@@ -0,0 +1,13 @@
+package com.cl.android.music.swipemenulistview;
+
+
+/**
+ * 
+ * @author baoyz
+ * @date 2014-8-24
+ *
+ */
+public interface SwipeMenuCreator {
+
+	void create(SwipeMenu menu);
+}
